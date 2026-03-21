26891f731bb61721e14baf0da9f311d0f84dec81
JamesKanneh <j.kannehii@alustudent.com>
2026-03-21 15:53:31 +0200

    Increase nginx ulimit to 65536

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fix Nginx to handle more requests by increasing file descriptor limit
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && nginx -s reload',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 65536\"/" /etc/default/nginx && nginx -s reload',
   path    => '/usr/local/bin/:/bin/:/sbin/'
 }
